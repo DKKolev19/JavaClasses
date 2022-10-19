126dca5218288b9c32f871309b3c132e1fae6801
Daniel Kolev <dkkolev19@codingburgas.bg>
2022-10-19 12:57:05 +0300

    Initial commit

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,5 +1,9 @@
 public class Car {
     private String brand;
     private String model;
-    public void start(){}
+    public void start(String brand,String model){
+        this.brand = brand;
+        this.model = model;
+        System.out.println("The brand of the car is " + brand + " and the model is " + model);
+    }
 }
